40bbfd3ed17c33adc4ef279ee2b81ca2f1a65684
metacollin <metacollin@electropi.mp>
2023-06-12 00:28:42 -0600

    moving stuff around

--- .mocharc.js
+++
@@ -1,8 +0,0 @@
-// mocha configuration
-'use strict'
-
-module.exports = {
-  require: ['scripts/init-test-env'],
-  'watch-extensions': ['js', 'ts', 'tsx', 'json'],
-  spec: ['apps/**/__tests__/*.@(js|ts|tsx)', 'packages/**/*test.js'],
-}
